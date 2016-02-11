78a11220face0e69312191d0b3b870da63dc8769
andresgz <andresgz@gmail.com>
2016-02-10 21:55:04 -0500

    Modify YoutubeBackground plugin to call https Youtube video

--- js/jquery.youtubebackground.js
+++ js/jquery.youtubebackground.js
@@ -26,7 +26,7 @@ if (typeof Object.create !== "function") {
       var tag = document.createElement('script'),
       head = document.getElementsByTagName('head')[0];
 
-      tag.src = 'http://www.youtube.com/iframe_api';
+      tag.src = 'https://www.youtube.com/iframe_api';
 
       head.appendChild(tag);
 
